7d9f4e2a0429da392f8a34823b32141ea059619c
OlaBooka <booka@yandex.ru>
2017-12-01 16:06:21 +0300

    tried multi string as template string

--- src/templates.js
+++ src/templates.js
@@ -1,5 +1,16 @@
 function greet(name) {
-    console.log(`hello ${name}`);
+    console.log(`hello ${name}`.toUpperCase());
 }
 
 greet('bill');
+
+function createEmail(to, from, object, message) {
+    console.log(`
+       to: ${to}
+       from: ${from}
+       object: ${object}
+       message: ${message}
+    `)
+}
+
+createEmail('jhon@mail.ru', 'jane@mail.ru', 'hello', 'how are you doing?');
