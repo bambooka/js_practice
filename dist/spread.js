0eaaf9ff0a2faed33433203f70d57a12f27cd807
OlaBooka <booka@yandex.ru>
2017-12-01 15:30:19 +0300

    add function and array for addition

--- dist/spread.js
+++ dist/spread.js
@@ -6,3 +6,9 @@ var dynamiclanguages = ["javaScript", "PHP", "Ruby"];
 var languages = [].concat(staticlanguages, ["c#"], dynamiclanguages, ["python"]);
 
 console.log(languages);
+
+function add(a, b, c) {
+    console.log(a + b + c);
+}
+
+var numbers = [1, 2, 3];
